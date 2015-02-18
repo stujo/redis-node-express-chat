7397d11cce3e68e990e861592290d38fec4f205c
stujo <stujo@users.noreply.github.com>
2015-02-18 10:40:04 -0800

    Long Polling

--- app.js
+++ app.js
@@ -2,17 +2,36 @@ var express = require("express");
 var fs = require('fs');
 var http = require('http');
 var path = require('path');
+var bodyParser = require('body-parser')
 
 var app = express();
 app.set('port', 3000);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
+app.use(bodyParser.json());
 
 // Serve up our static resources
 app.get('/', function(req, res) {
   fs.readFile('./public/index.html', function(err, data) {
     res.end(data);
   });
+});
+
+var clients = [];
+
+// Poll endpoint
+app.get('/poll/*', function(req, res) {
+  clients.push(res);
+});
+
+// Msg endpoint
+app.post('/msg', function(req, res) {
+  message = req.body;
+  var msg = JSON.stringify(message);
+  while(clients.length > 0) {
+    var client = clients.pop();
+    client.end(msg);
+  }
+  res.end();
 });
 
 http.createServer(app).listen(app.get('port'), function(){
--- public/client.js
+++ public/client.js
@@ -1,4 +1,59 @@
  $(document).ready(function() {
+   var name = '';
+   var polling = false;
 
+   function go() {
+     name = $('#user-name').val();
+     $('#user-name').val('');
+     $('.user-form').hide();
+     $('.chat-box').show();
+     poll();
+   };
+
+   function poll() {
+     $.getJSON('/poll/' + new Date().getTime(), function(response, statusText, jqXHR) {
+       if (jqXHR.status == 200) {
+         $('.jumbotron').hide();
+         msg = response;
+         var html = '<div class="panel panel-success"><div class="panel-heading"><h3 class="panel-title">' +
+           msg.username +
+           '</h3></div><div class="panel-body">' + msg.message + '</div></div>';
+         var d = $('.message-area');
+         d.append(html);
+         d.scrollTop(d.prop("scrollHeight"));
+       }
+       poll();
+     });
+   };
+
+   $('#user-name').keydown(function(e) {
+     if (e.keyCode == 13) { //Enter pressed
+       go();
+     }
+   });
+
+   $('.go-user').on('click', function(e) {
+     go();
+   });
+
+   $('#message-input').keydown(function(e) {
+     if (e.keyCode == 13) {
+       $.ajax({
+         type: "POST",
+         url: "/msg",
+         data: JSON.stringify({
+           "username": name,
+           "message": $('#message-input').val().trim()
+         }),
+         contentType: "application/json"
+       });
+       $(this).val('');
+       $('.jumbotron').hide();
+       e.preventDefault()
+     }
+   });
+
+
+
+   $('.user-form').show();
  });
-
